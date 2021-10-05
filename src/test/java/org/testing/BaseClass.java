8c2a1bfeed61a0c7099e91587d830bfd540e309b
Sarath13-dev <ramsssjrs@gmail.com>
2021-10-05 23:40:38 +0530

    second commit

--- src/test/java/org/testing/BaseClass.java
+++ src/test/java/org/testing/BaseClass.java
@@ -10,6 +10,7 @@ public class BaseClass {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\RAM SARATH KUMAR\\eclipse-workspace\\JunitLearnMorning\\driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get(url);
+		System.out.println("kumar");
 		
 	}
 	
