064194bc72d3ef0de50a7d63520b3f26d03b2de2
Sarath13-dev <ramsssjrs@gmail.com>
2021-10-05 09:30:50 +0530

    first commit

---
+++ src/test/java/org/testing/BaseClass.java
@@ -0,0 +1,16 @@
+package org.testing;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseClass {
+
+	public static void launchBrowser(String url) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\RAM SARATH KUMAR\\eclipse-workspace\\JunitLearnMorning\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get(url);
+		
+	}
+	
+}
---
+++ src/test/java/org/testing/TestLearn.java
@@ -0,0 +1,19 @@
+package org.testing;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class TestLearn extends BaseClass {
+	
+	@Test
+	public void tc1() {
+		launchBrowser("https://en-gb.facebook.com/");
+		System.out.println("test1 - @Test");
+	}
+	
+
+}
